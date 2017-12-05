7ef80caa6d405cf22fc5d897046af1ef51691549
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-12-05 13:11:21 +0100

    work-in-progress to use clang on windows

--- build.ps1
+++ build.ps1
@@ -4,14 +4,10 @@ mkdir -Force build
 cd build
 
 #dynamically linked
-cmake .. -T"LLVM-vs2015" -G "Visual Studio 15" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DCMAKE_LINKER=clang.exe" "-DCMAKE_CXX_COMPILER=clang++.exe" "-DCMAKE_C_COMPILER=clang.exe" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release"
-#cmake .. -G "Ninja" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DCMAKE_LINKER=clang.exe" "-DCMAKE_CXX_COMPILER=clang-cl.exe" "-DCMAKE_C_COMPILER=clang-cl.exe" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release"
-#cmake .. -G "Ninja" "-DCMAKE_CXX_COMPILER=clang-cl.exe" "-DCMAKE_C_COMPILER=clang-cl.exe" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release"
-#cmake .. -G "Ninja" "-DCMAKE_CXX_COMPILER=cl.exe" "-DCMAKE_C_COMPILER=cl.exe" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release"
-#cmake .. -G "Visual Studio 15" -T"v141_clang_c2" "-DCMAKE_CXX_COMPILER=cl.exe" "-DCMAKE_C_COMPILER=cl.exe" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release"
-
-#statically linked
-#cmake .. -G "Ninja" "-DCMAKE_CXX_COMPILER=cl.exe" "-DCMAKE_C_COMPILER=cl.exe" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release"
+#cmake .. -T"LLVM-vs2015" -G "Visual Studio 15" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DCMAKE_LINKER=clang.exe" "-DCMAKE_CXX_COMPILER=clang++.exe" "-DCMAKE_C_COMPILER=clang.exe" "-DCMAKE_BUILD_TYPE=Release"
+#cmake .. -G "Ninja" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DCMAKE_LINKER=clang.exe" "-DCMAKE_CXX_COMPILER=clang++.exe" "-DCMAKE_C_COMPILER=clang.exe" "-DCMAKE_BUILD_TYPE=Release"
+#cmake .. -G "Ninja" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DCMAKE_LINKER=clang-cl.exe" "-DCMAKE_CXX_COMPILER=clang-cl.exe" "-DCMAKE_C_COMPILER=clang-cl.exe" "-DCMAKE_BUILD_TYPE=Release"
+cmake .. -G "Ninja" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DCMAKE_LINKER=cl.exe" "-DCMAKE_CXX_COMPILER=cl.exe" "-DCMAKE_C_COMPILER=cl.exe" "-DCMAKE_BUILD_TYPE=Release"
 
 cmake --build . #--config Release
 
