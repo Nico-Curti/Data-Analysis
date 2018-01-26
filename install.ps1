c8f43ebfbfacbf1a469edddab04d5fd6c4af6210
Nico Curti <nico.curti2@unibo.it>
2018-01-26 13:05:53 +0100

    new bash

--- install.ps1
+++ install.ps1
@@ -4,8 +4,9 @@
 # Set-ExecutionPolicy Bypass -Scope CurrentUser
 $cmake_version = "3.10.1"
 $ninja_version = "1.8.2"
+$Documents = [Environment]::GetFolderPath('MyDocuments')
 
-Write-Host "Installing Data-Analysis dependecies:"
+Write-Host "Installing Data-Analysis dependecies:" -ForegroundColor Yellow
 Write-Host "- cmake"
 . ".\shell_utils\pwsh\install_cmake.ps1"
 Write-Host "- Python3 (snakemake)"
@@ -22,40 +23,42 @@ Write-Host "- Ninja"
 
 If( $args[1] -eq $null ) { $path2out = "toolchain" } # specify path to install programs
 Else {  $path2out = $args[1] }
+Write-Host "Installation path : $env:HOMEDRIVE$env:HOMEPATH\$path2out" -ForegroundColor Yellow
+
 
 Push-Location
 Set-Location $env:HOMEDRIVE$env:HOMEPATH
 New-Item -Path $path2out -ItemType directory -Force > $null
-New-Item -Path $env:UserProfile\Documents\WindowsPowerShell -ItemType directory -Force > $null
+New-Item -Path $Documents\WindowsPowerShell -ItemType directory -Force > $null
 Set-Location $path2out
 
 
 Write-Host "Looking for packages..."
 
 # CMAKE Installer
-Write-Host "CMAKE identification"
+Write-Host "cmake identification"
 If( -Not (Get-Command cmake -ErrorAction SilentlyContinue) ){ # cmake not installed
-    Write-Host CMAKE not FOUND
+    Write-Host "cmake not FOUND" -ForegroundColor Red
     $up_version = $cmake_version.Substring(0, $cmake_version.Length - 2)
     If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ){ install_cmake "https://cmake.org/files/v$up_version/cmake-$cmake_version-win64-x64.zip" "." $true }
     Else{
         $CONFIRM = Read-Host -Prompt "Do you want install it? [y/n]"
-        If($CONFIRM -eq "N" -Or $CONFIRM -eq "n"){ Write-Host Abort CMAKE installation }
+        If($CONFIRM -eq "N" -Or $CONFIRM -eq "n"){ Write-Host "Abort" -ForegroundColor Red }
         Else{ install_cmake "https://cmake.org/files/v$up_ver/cmake-$cmake_ver-win64-x64.zip" "." $true }
     }
 }
-Else{ Write-Host CMAKE FOUND }
+Else{ Write-Host "cmake FOUND" -ForegroundColor Green}
 
 
 # Miniconda3 Installer
-Write-Host Python3 identification
+Write-Host "Python3 identification"
 If( -Not (Get-Command python -ErrorAction SilentlyContinue) ){ $pyver = "" }
 Else {  $pyver = python -c "import sys; print(sys.version)" }# python version
 If($pyver -like "*3.*" -Or $pyver -like "*Anaconda 3.*") {# right version 3. so install snakemake
-    Write-Host Python3 FOUND
-    Write-Host snakemake identification
+    Write-Host "Python3 FOUND" -ForegroundColor Green
+    Write-Host "snakemake identification"
     If( -Not (Get-Command snakemake -ErrorAction SilentlyContinue) ) {
-        Write-Host snakemake not FOUND
+        Write-Host "snakemake not FOUND" -ForegroundColor Red
         If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ) {
             conda update conda -y
             conda config --add channels bioconda
@@ -63,7 +66,7 @@ If($pyver -like "*3.*" -Or $pyver -like "*Anaconda 3.*") {# right version 3. so 
         }
         Else{
             $CONFIRM = Read-Host -Prompt "Do you want install snakemake and other dependecies? [y/n]"
-            If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host Abort }
+            If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host "Abort" -ForegroundColor Red }
             Else{
                 conda update conda -y
                 conda config --add channels bioconda
@@ -71,20 +74,20 @@ If($pyver -like "*3.*" -Or $pyver -like "*Anaconda 3.*") {# right version 3. so 
             }
         }
     }
-    Else { Write-Host snakemake FOUND }
+    Else { Write-Host "snakemake FOUND" -ForegroundColor Green}
 }
 ElseIf($pyver -like "*2.*" -Or $pyver -like "*Anaconda 2.*") {
-    Write-Host The Python version found is too old for snakemake
-    Write-Host Please set a new version of Python as default
-    Write-Host OR
-    Write-Host Uninstall the current version and re-run this script for a Python3 installation
+    Write-Host "The Python version found is too old for snakemake" -ForegroundColor Red
+    Write-Host "Please set a new version of Python as default" -ForegroundColor Red
+    Write-Host "OR" -ForegroundColor Red
+    Write-Host "Uninstall the current version and re-run this script for a Python3 installation" -ForegroundColor Red
 }
 Else{
-    Write-Host Python not FOUND
+    Write-Host "Python not FOUND" -ForegroundColor Red
     If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ) { install_python "https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe" $true seaborn pandas ipython numpy matplotlib snakemake graphviz spyder}
     Else{
         $CONFIRM = Read-Host -Prompt "Do you want install snakemake and other dependecies? [y/n]"
-        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host Abort }
+        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host "Abort" -ForegroundColor Red }
         Else { install_python "https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe" $true seaborn pandas ipython numpy matplotlib snakemake graphviz spyder}
     }
 }
@@ -95,43 +98,43 @@ $gcc = Get-Command g++ -ErrorAction SilentlyContinue | Select-Object -ExpandProp
 If( $gcc -eq $null){$version = ""}
 Else{$version = & g++ "--version"}
 If( $gcc -eq $null ){ # g++ not found
-    Write-Host g++ not installed
+    Write-Host "g++ not installed" -ForegroundColor Red
     If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ){ install_g++ "." $true }
     Else{
         $CONFIRM = Read-Host -Prompt "Do you want install it? [y/n]"
-        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host Abort }
+        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host "Abort" -ForegroundColor Red }
         Else{ install_g++ "." $true }
     }
 }
 ElseIf( $version.split(' ')[6].split('.')[0] -lt 4 ){ # version too old
-    Write-Host g++ version too old for OpenMP 4
+    Write-Host "g++ version too old for OpenMP 4" -ForegroundColor Red
     If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ){ install_g++ "." $true }
     Else{
         $CONFIRM = Read-Host -Prompt "Do you want install it? [y/n]"
-        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host Abort }
+        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host "Abort" -ForegroundColor Red}
         Else{ 
             install_g++ "." $true 
             $env:PATH = $env:PATH - ";$gcc"
-            -join('$env:PATH = $env:PATH', " - `";$gcc`"") | Out-File -FilePath "$env:UserProfile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Append -Encoding ASCII
+            -join('$env:PATH = $env:PATH', " - `";$gcc`"") | Out-File -FilePath "$env:UserProfile\$Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Append -Encoding ASCII
         }
     }
 }
-Else{ Write-Host "A sufficient version of g++ already installed" }
+Else{ Write-Host "A sufficient version of g++ already installed" -ForegroundColor Green}
 
 
 # Chocolatey Installer
 Write-Host Chocolatey identification
 $choco = Get-Command choco -ErrorAction SilentlyContinue
 If( $choco -eq $null ){
-    Write-Host Chocolatey not FOUND
+    Write-Host "Chocolatey not FOUND" -ForegroundColor Red
     If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ) { install_choco }
     Else{
         $CONFIRM = Read-Host -Prompt "Do you want install it? [y/n]"
-        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host Abort }
+        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host "Abort" -ForegroundColor Red}
         Else{ install_choco }
     }
 }
-Else { Write-Host Choco FOUND}
+Else { Write-Host Choco FOUND -ForegroundColor Green}
 
 
 # Sublime Installer
@@ -142,27 +145,27 @@ If( $sub -eq $false){ # not installed
     If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ){ install_sublime "." $true }
     Else{
         $CONFIRM = Read-Host -Prompt "Do you want install it? [y/n]"
-        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host Abort }
+        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host "Abort" -ForegroundColor Red}
         Else{ install_sublime "." $true }
     }
 }
-Else{ Write-Host SublimeText3 already installed }
+Else{ Write-Host SublimeText3 already installed -ForegroundColor Green}
 
 # Ninja-build Installer
 Write-Host ninja identification
 If( -Not (Get-Command ninja -ErrorAction SilentlyContinue) ){ # ninja not installed
-    Write-Host ninja not FOUND
+    Write-Host "ninja not FOUND" -ForegroundColor Red
     If( $args[0] -eq "-y" -Or $args[0] -eq "-Y" -Or $args[0] -eq "yes" ){ install_ninja "https://github.com/ninja-build/ninja/releases/download/v$ninja_version/ninja-win.zip" "." $true }
     Else{
         $CONFIRM = Read-Host -Prompt "Do you want install it? [y/n]"
-        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host Abort }
+        If($CONFIRM -eq 'N' -Or $CONFIRM -eq 'n') { Write-Host "Abort" -ForegroundColor Red}
         Else{ install_ninja "https://github.com/ninja-build/ninja/releases/download/v$ninja_version/ninja-win.zip" "." $true }
     }
 }
-Else{ Write-Host ninja FOUND }
+Else{ Write-Host "ninja FOUND" -ForegroundColor Green}
 
 Pop-Location
 
-Write-Host "Build Data-Analysis libraries with cmake-ninja"
+Write-Host "Build Data-Analysis libraries with cmake-ninja" -ForegroundColor Yellow
 & .\build.ps1
 
--- install.sh
+++ install.sh
@@ -1,342 +1,123 @@
 #!/bin/bash
+source ~/.bashrc
 
-echo "Installing Data-Analysis dependecies:"
-echo "- g++ (> 4.9)"
-echo "- make"
-echo "- 7zip"
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+cmake_version="3.10.1"
+ninja_version="1.8.2"
+
+echo ${yellow}"Installing Data-Analysis dependecies:"
 echo "- cmake"
+source ./shell_utils/bash/install_cmake.sh
+echo "- 7zip"
+source ./shell_utils/bash/install_7zip.sh
 echo "- Ninja"
+source ./shell_utils/bash/install_ninja.sh
+echo "- g++ (> 4.9)"
+source ./shell_utils/bash/install_g++.sh
+echo "- make"
+source ./shell_utils/bash/install_make.sh
 echo "- Python3 (snakemake)"
+source ./shell_utils/bash/install_python.sh
 
-if [ "$2" == "" ]; then
-	path2out="toolchain"
-else
-	path2out=$2
-fi
+
+if [ "$2" == "" ]; then	path2out="toolchain"; else path2out=$2; fi
+echo ${yellow}"Installation path : "~/$path2out
 
 pushd $HOME
 mkdir -p $path2out
 cd $path2out
 
-source ~/.bashrc
-
 echo "Looking for packages..."
-echo "C++ compiler identification (g++ version greater than 4.9)"
+# mkdir -p var/lib/dpkg/updates
+# touch var/lib/dpkg/status
+# mkdir ./var/lib/dpkg/info
+# touch ./var/lib/dpkg/info/format
+# echo 1 >> ./var/lib/dpkg/info/format
+# dpkg --force-not-root --root=$HOME/tmp_gcc --log ./log.dpkg --install make_4.1-6_amd64.deb
 
-install_make=false
-## install make
-#if [ ! -x make ]; then
-#	echo "installing temporary version of make"
-#	install_make=1
-#	apt-get download make
-#	mkdir tmp_make
-#	dpkg -x make*.deb tmp_make
-#	rm -rf make*.deb
-#	export PATH=$PATH:$PWD/tmp_make/usr/bin
-#	#echo export PATH='$PATH':$PWD/tmp_make/usr/bin >> ~/.bashrc
-#fi
-install_gcc=false
-## install g++
-#if [ ! -x g++ ]; then
-#	echo "installing temporary version of g++"
-#	#wget "http://releases.llvm.org/5.0.1/clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
-#	#mkdir tmp_compiler
-#	#mv clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz tmp_compiler/
-#	#cd tmp_compiler
-#	#tar xvf clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-#	#rm clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-#	#export CXX=$PWD/clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04/bin/clang++
-#	#export CC=$PWD/clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04/bin/clang
-#	#export PATH=$PATH:$PWD/clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04/bin/
-#	install_gcc=1
-#	apt-get download cpp 
-#	versione=$(find . -name "*.deb" | cut -d'a' -f 2 | cut -d'.' -f 1)
-#	apt-get download binutils cpp-$versione g++-$versione gcc gcc-$versione gcc-$versione-base libasan2 libatomic1 libc-dev-bin libc6 libc6-dev libcc1-0 libcilkrts5 libgcc-$versione-dev libgomp1 libisl15 libitm1 liblsan0 libmpc3 libmpx0 libquadmath0 libstdc++-$versione-dev libstdc++6 libtsan0 libubsan0 linux-libc-dev manpages-dev
-#	mkdir tmp_gcc
-#	for file in $(find . -name "*.deb"); do 
-#		dpkg -x $file tmp_gcc
-#		rm -rf $file
-#	done
-#	export LD_LIBRARY_PATH=$PWD/tmp_gcc/usr/lib/x86_64-linux-gnu/:$PWD/tmp_gcc/usr/lib/gcc/x86_64-linux-gnu/$versione/:$LD_LIBRARY_PATH
-#	export LIBRARY_PATH=$PWD/tmp_gcc/usr/lib/x86_64-linux-gnu/:$PWD/tmp_gcc/usr/lib/gcc/x86_64-linux-gnu/$versione/:$LIBRARY_PATH
-#	export COMPILER_PATH=$PWD/tmp_gcc/usr/lib/x86_64-linux-gnu/:$PWD/tmp_gcc/usr/lib/gcc/x86_64-linux-gnu/$versione/:$COMPILER_PATH
-#	#export GCC_EXEC_PREFIX=$PWD/tmp_gcc/
-#	export C_INCLUDE_PATH=$PWD/tmp_gcc/usr/include/:$C_INCLUDE_PATH
-#	export C_INCLUDE_PATH=$PWD/tmp_gcc/usr/include/x86_64-linux-gnu/:$C_INCLUDE_PATH
-#	export CPLUS_INCLUDE_PATH=$PWD/tmp_gcc/usr/include/c++/$versione/:$C_INCLUDE_PATH:$CPLUS_INCLUDE_PATH
-#	export PATH=$PATH:$PWD/tmp_gcc/usr/bin/
-#	export CC=$PWD/tmp_gcc/usr/bin/gcc-5
-#	export CXX=$PWD/tmp_gcc/usr/bin/g++-5
-#	#echo export PATH='$PATH':$PWD/tmp_gcc/usr/bin >> ~/.bashrc
-#fi
-
-
-
-# install gcc new version for OpenMP 4.0 support
-if [[ -x "/usr/bin/g++" ]]; then GCCVER=$(g++ --version | awk '/g++ /{print $0;exit 0;}' | cut -d' ' -f 4 | cut -d'.' -f 1 ); fi
-if [[ "$GCCVER" -lt "5" ]]; then
-	echo "g++ version too old or not installed"
-	if [ "$1" == "-y" ] || [ "$1" == "-Y" ] || [ "$1" == "yes" ]; then
-		# install tmp version of gcc
-		if [ -x "/usr/bin/gcc" ]; then
-			echo "Download a temporary version of gcc"
-			wget "ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.gz"
-			tar xzf gcc-4.9.4.tar.gz
-			mv gcc-4.9.4 gcc-4.9.4-sources
-			cd gcc-4.9.4-sources
-			./contrib/download_prerequisites
-			cd ..
-			mkdir tmp_gcc
-			cd tmp_gcc
-			$PWD/../gcc-4.9.4-sources/configure --prefix=$HOME/gcc-4.9.4 --enable-languages=c,c++
-			make
-			make install
-			#rm -rf tmp_gcc gcc-4.9.4-sources
-			export CC=$HOME/gcc-4.9.4/bin/gcc
-			export CXX=$HOME/gcc-4.9.4/bin/g++
-			cd ..
-		else
-			echo "gcc compiler NOT FOUND!"
-			exit
-		fi
-
-
-		# install modern gcc
-		wget "ftp://ftp.gnu.org/gnu/gcc/gcc-7.2.0/gcc-7.2.0.tar.gz"
-		tar xzf gcc-7.2.0.tar.gz
-		mv gcc-7.2.0 gcc-7.2.0-sources
-		cd gcc-7.2.0-sources
-		./contrib/download_prerequisites
-		cd ..
-		mkdir objdir
-		cd objdir
-		$PWD/../gcc-7.2.0-sources/configure --prefix=$HOME/gcc-7.2.0 --enable-languages=c,c++
-		make
-		make install
-		#rm -rf objdir gcc-7.2.0-sources
-		#if [[ "$GCCVER" -lt "5" ]] || $install_gcc; then
-		#	rm -rf ../tmp_gcc
-		#fi
-		export CC=$HOME/gcc-7.2.0/bin/gcc
-		export CXX=$HOME/gcc-7.2.0/bin/g++
-		echo "export CC=$HOME/gcc-7.2.0/bin/gcc" >> ~/.bashrc
-		echo "export CXX=$HOME/gcc-7.2.0/bin/g++" >> ~/.bashrc
-		export PATH=$PATH:$PWD/gcc-7.2.0/bin/
-		echo export PATH='$PATH':$PWD/gcc-7.2.0/bin/ >> ~/.bashrc
-		cd ..
-
+# CMAKE Installer
+echo "CMAKE identification"
+if [ ! -x "cmake" ]; then
+	echo ${red}CMAKE not FOUND
+	up_version=${cmake_version::-2}
+	if [ "$3" == "-y" ] || [ "$3" == "-Y" ] || [ "$3" == "yes" ]; then install_cmake "https://cmake.org/files/v$up_version/cmake-$cmake_version-Linux-x86_64.tar.gz" "." true;
 	else
-		read -p "Do you want install it? [y/n] " CONFIRM
-		if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then
-			echo "Abort gcc installation"
-		else
-			# install tmp version of gcc
-			if [ -x "/usr/bin/gcc" ]; then
-				echo "Download a temporary version of gcc"
-				wget "ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.gz"
-				tar xzf gcc-4.9.4.tar.gz
-				mv gcc-4.9.4 gcc-4.9.4-sources
-				cd gcc-4.9.4-sources
-				./contrib/download_prerequisites
-				cd ..
-				mkdir tmp_gcc
-				cd tmp_gcc
-				$PWD/../gcc-4.9.4-sources/configure --prefix=$HOME/gcc-4.9.4 --enable-languages=c,c++
-				make
-				make install
-				#rm -rf tmp_gcc gcc-4.9.4-sources
-				export CC=$HOME/gcc-4.9.4/bin/gcc
-				export CXX=$HOME/gcc-4.9.4/bin/g++
-				cd ..
-			else
-				echo "gcc compiler NOT FOUND!"
-				exit
-			fi
-
-			# install modern gcc
-			wget "ftp://ftp.gnu.org/gnu/gcc/gcc-7.2.0/gcc-7.2.0.tar.gz"
-			tar xzf gcc-7.2.0.tar.gz
-			mv gcc-7.2.0 gcc-7.2.0-sources
-			cd gcc-7.2.0-sources
-			./contrib/download_prerequisites
-			cd ..
-			mkdir objdir
-			cd objdir
-			$PWD/../gcc-7.2.0-sources/configure --prefix=$HOME/gcc-7.2.0 --enable-languages=c,c++
-			make
-			make install
-			#rm -rf objdir gcc-7.2.0-sources
-			#if [[ "$GCCVER" -lt "5" ]] || $install_gcc; then
-			#	rm -rf ../tmp_gcc
-			#fi
-			export CC=$HOME/gcc-7.2.0/bin/gcc
-			export CXX=$HOME/gcc-7.2.0/bin/g++
-			echo "export CC=$HOME/gcc-7.2.0/bin/gcc" >> ~/.bashrc
-			echo "export CXX=$HOME/gcc-7.2.0/bin/g++" >> ~/.bashrc
-			export PATH=$PATH:$PWD/gcc-7.2.0/bin/
-			echo export PATH='$PATH':$PWD/gcc-7.2.0/bin/ >> ~/.bashrc
-			cd ..
+		read -p "Do you want install it? [y/n] " confirm
+		if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then echo ${red}"Abort";
+		else install_cmake "https://cmake.org/files/v$up_version/cmake-$cmake_version-Linux-x86_64.tar.gz" "." true;
 		fi
 	fi
-else
-	echo "gcc already installed"
-fi
-exit
-
-if [[ "$(which 7zip)" == "" ]]; then
-	wget https://downloads.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2
-	tar jxvf p7zip_16.02_src_all.tar.bz2
-	cd p7zip_16.02/
-	cp makefile.linux_gcc6_sanitize makefile.linux
-	make -j all_test
-	cd ..
-	export PATH=$PATH:$PWD/p7zip_16.02/bin
-	echo export PATH='$PATH':$PWD/p7zip_16.02/bin >> ~/.bashrc
-	#./bin/7za
+else echo ${green}"CMAKE FOUND";
 fi
 
-function install # $1 program name, $2 url, $3 confirm
-{
-	echo $1 "identification"
-	PROG="$(which $1)"
-	if [ "$PROG" == "" ]; then
-		echo $1 "not FOUND"
-		if [ "$3" == "-y" ] || [ "$3" == "-Y" ] || [ "$3" == "yes" ]; then
-			echo "Download $1 from $2"
-			IFS='/' read -ra TOKEN <<< "$2"
-			OUT_DIR=${TOKEN[${#TOKEN[@]}-1]}
-			IFS='-' read -ra OUT <<< "$OUT_DIR"
-			wget $2
-			if [[ "$OUT_DIR" == *".tar.gz" ]]; then
-				echo "Unzip" $OUT_DIR 
-				tar zxf $OUT_DIR
-				OUT="${OUT_DIR%.*}"
-				OUT="${OUT%.*}"
-			elif [[ "$OUT_DIR" == *".zip" ]]; then
-				echo "Unzip" $OUT_DIR
-				7za x "$OUT_DIR"
-				OUT="${OUT_DIR%.*}"
-			elif [[ "OUT_DIR" == *".tar.bz2" ]]; then
-				echo "Unzip" $OUT_DIR
-				tar jxf $OUT_DIR
-				OUT="${OUT_DIR%.*}"
-			else
-				echo "already uncompressed"
-				OUT="$OUT_DIR"
-			fi
-			rm -rf "$OUT_DIR"*
-			IFS="-" read -ra OUT_DIR <<< "$OUT"
-			mv ${OUT} $OUT_DIR
-			export PATH=$PATH:$PWD/$OUT_DIR/bin
-			echo export PATH='$PATH':$PWD/$OUT_DIR/bin >> ~/.bashrc
+# 7zip Installer
+if [ ! -x "unzip" ]; then
+	echo "7zip identification"
+	if [ ! -x "7za" ]; then
+		echo ${red}7zip not FOUND
+		if [ "$3" == "-y" ] || [ "$3" == "-Y" ] || [ "$3" == "yes" ]; then install_7zip;
 		else
-			read -p "Do you want install it? [y/n] " CONFIRM
-			if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then
-				echo "Abort" $1 "installation"
-			else
-				echo "Download $1 from $2"
-				IFS='/' read -ra TOKEN <<< "$2"
-				OUT_DIR=${TOKEN[${#TOKEN[@]}-1]}
-				IFS='-' read -ra OUT <<< "$OUT_DIR"
-				wget $2
-				if [[ "$OUT_DIR" == *".tar.gz" ]]; then
-					echo "Unzip" $OUT_DIR 
-					tar zxf $OUT_DIR
-					OUT="${OUT_DIR%.*}"
-					OUT="${OUT%.*}"
-				elif [[ "$OUT_DIR" == *".zip" ]]; then
-					echo "Unzip" $OUT_DIR
-					7za x "$OUT_DIR"
-					OUT="${OUT_DIR%.*}"
-				elif [[ "OUT_DIR" == *".tar.bz2" ]]; then
-					echo "Unzip" $OUT_DIR
-					tar jxf $OUT_DIR
-					OUT="${OUT_DIR%.*}"
-				else
-					echo "already uncompressed"
-					OUT="$OUT_DIR"
-				fi
-				rm -rf "$OUT_DIR"*
-				IFS="-" read -ra OUT_DIR <<< "$OUT"
-				mv ${OUT} $OUT_DIR
-				export PATH=$PATH:$PWD/$OUT_DIR/bin
-				echo export PATH='$PATH':$PWD/$OUT_DIR/bin >> ~/.bashrc
+			read -p "Do you want install it? [y/n] " confirm
+			if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then echo ${red}"Abort";
+			else install_7zip;
 			fi
 		fi
-	else
-		echo $1 "FOUND"
+	else echo ${green}"7zip FOUND";
 	fi
-}
+fi
 
-#export OMP_CANCELLATION=true
-install "cmake" "https://cmake.org/files/v3.10/cmake-3.10.0-Linux-x86_64.tar.gz" "$1"
-install "ninja" "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip" "$1"
+# Ninja Installer
+echo "Ninja-build identification"
+if [ ! -x "ninja" ]; then
+	echo ${red}Ninja not FOUND
+	if [ "$3" == "-y" ] || [ "$3" == "-Y" ] || [ "$3" == "yes" ]; then install_ninja "https://github.com/ninja-build/ninja/releases/download/v$ninja_version/ninja-linux.zip" "." true;
+	else
+		read -p "Do you want install it? [y/n] " confirm
+		if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then echo ${red}"Abort";
+		else install_ninja "https://github.com/ninja-build/ninja/releases/download/v$ninja_version/ninja-linux.zip" "." true;
+		fi
+	fi
+else echo ${green}"CMAKE FOUND";
+fi
 
-# install python
+# Python3 Installer
 echo "Python3 and snakemake identification"
-PROG="$(which python)"
-if [ "$PROG" == "" ]; then
-	echo "Python not FOUND"
-	if [ "$1" == "-y" ] || [ "$1" == "-Y" ] || [ "$1" == "yes" ]; then
-		echo "Download Miniconda3 from "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-		wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-		chmod ugo+x Miniconda3-latest-Linux-x86_64.sh 
-		bash ./Miniconda3-latest-Linux-x86_64.sh -b
-		conda update conda -y
-		conda config --add channels bioconda
-		pip install -U numpy matplotlib seaborn networkx scikit-learn pandas snakemake graphviz
-		rm ./Miniconda3-latest-Linux-x86_64.sh
-		export PATH=~/miniconda3/bin:$PATH 
-		echo export PATH=~/miniconda3/bin:~/miniconda3/Scripts:~/miniconda3/Library/bin:~/miniconda3/Library/usr/bin:~/miniconda3/Library/mingw-w64/bin:$PATH >> ~/.bashrc
-	else
-		read -p "Do you want install it? [y/n] " CONFIRM
-		if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then
-			echo "Abort Python installation"
-		else
-			echo "Download Miniconda3 from "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-			wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-			chmod ugo+x Miniconda3-latest-Linux-x86_64.sh 
-			bash ./Miniconda3-latest-Linux-x86_64.sh -b
+if [ "$(python -c 'import sys;print(sys.version_info[0])')" -eq "3" ]; then
+	echo ${green}"Python FOUND"
+	echo "snakemake identification"
+	if [ ! -x "snakemake" ]
+		echo ${red}"snakemake not FOUND"
+		if [ "$3" == "-y" ] || [ "$3" == "-Y" ] || [ "$3" == "yes" ]; then
 			conda update conda -y
 			conda config --add channels bioconda
-			pip install -U numpy matplotlib seaborn networkx scikit-learn pandas snakemake graphviz
-			rm ./Miniconda3-latest-Linux-x86_64.sh
-			export PATH=~/miniconda3/bin:$PATH 
-			echo export PATH=~/miniconda3/bin:~/miniconda3/Scripts:~/miniconda3/Library/bin:~/miniconda3/Library/usr/bin:~/miniconda3/Library/mingw-w64/bin:$PATH >> ~/.bashrc
-		fi
+            pip install seaborn pandas ipython numpy matplotlib snakemake graphviz spyder
+        else
+        	read -p "Do you want install it? [y/n] " confirm
+        	if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then echo ${red}"Abort";
+        	else
+				conda update conda -y
+				conda config --add channels bioconda
+	            pip install seaborn pandas ipython numpy matplotlib snakemake graphviz spyder
+	        fi
+	    fi
+	else echo ${green}"snakemake FOUND"
 	fi
-elif [ "$(python -c 'import sys;print(sys.version_info[0])')" -eq "3" ]; then
-	echo "Python3 FOUND"
-	if [ "$1" == "-y" ] || [ "$1" == "-Y" ] || [ "$1" == "yes" ]; then
-		conda update conda -y
-		conda config --add channels bioconda
-		pip install -U numpy matplotlib seaborn networkx scikit-learn pandas snakemake graphviz
-	else
-		read -p "Do you want install snakemake and other dependecies? [y/n] " CONFIRM
-		if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then
-			echo "Abort"
-		else
-			conda update conda -y
-			conda config --add channels bioconda
-			pip install -U numpy matplotlib seaborn networkx scikit-learn pandas snakemake graphviz
-		fi
-	fi
+elif [ "$(python -c 'import sys;print(sys.version_info[0])')" -eq "2" ]; then
+	echo ${red}"The Python version found is too old for snakemake"
+	echo ${red}"Please set a new version of Python as default"
+	echo ${red}"OR"
+	echo ${red}"Uninstall the current version and re-run this script for a Python3 installation"
 else
-	echo "Current python version is too old for snakemake"
-	echo "Please uninstall your python and re-run this script."
+	echo ${red}"Python not FOUND"
+	if [ "$3" == "-y" ] || [ "$3" == "-Y" ] || [ "$3" == "yes" ]; then install_python "https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe" true seaborn pandas ipython numpy matplotlib snakemake graphviz spyder;
+	else
+		read -p "Do you want install it? [y/n] " confirm
+		if [ "$CONFIRM" == "n" ] || [ "$CONFIRM" == "N" ]; then echo ${red}"Abort";
+		else install_python "https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe" true seaborn pandas ipython numpy matplotlib snakemake graphviz spyder;
+		fi
+	fi
+else echo ${green}"Python3 FOUND";
 fi
-
-
-
-popd
-
-# Install Data-Analysis Lib
-echo "Build Data-Analysis libraries with cmake-ninja"
-
-rm -rf build
-mkdir -p build
-cd build
-
-cmake -G "Ninja" "-DCMAKE_BUILD_TYPE=Release" ..
-cmake --build .
-cd ..
